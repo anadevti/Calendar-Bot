bd7530c0d5c087ffad449e558c346154890bbb33
Ana Carolyne <111382055+anadevti@users.noreply.github.com>
2024-12-16 01:12:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
+from google.oauth2.service_account import Credentials
 import datetime
 import os
 import json
